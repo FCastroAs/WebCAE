62ef69f8c8239e482c1e6e66f2763b060ad31e2a
Fernán Castro <[fcastro@nalandaglobal.com](mailto:fcastro@nalandaglobal.com)>
2022-04-19 09:07:54 +0200

    Compilar utilizando UTF-8

--- src/test/java/edu/uoc/webcae/WebCaeApplicationTests.java
+++ src/test/java/edu/uoc/webcae/WebCaeApplicationTests.java
@@ -3,7 +3,7 @@ package edu.uoc.webcae;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WebCaeApplicationTests {
 
 	@Test
