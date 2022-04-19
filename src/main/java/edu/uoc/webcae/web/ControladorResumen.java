afb43acd4a8f274427d544114f6f5856c06ce7fb
Fernán Castro <[fcastro@nalandaglobal.com](mailto:fcastro@nalandaglobal.com)>
2022-04-19 07:34:38 +0200

    Limpieza de código

--- src/main/java/edu/uoc/webcae/web/ControladorResumen.java
+++ src/main/java/edu/uoc/webcae/web/ControladorResumen.java
@@ -1,5 +1,7 @@
 package edu.uoc.webcae.web;
 
+import edu.uoc.webcae.domain.Cliente;
+import edu.uoc.webcae.domain.Persona;
 import edu.uoc.webcae.domain.Usuario;
 import edu.uoc.webcae.servicio.ClienteService;
 import edu.uoc.webcae.servicio.UsuarioServiceImpl;
@@ -9,7 +11,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @Slf4j
@@ -27,5 +33,32 @@ public class ControladorResumen {
         var clientes = usuario.getClientes();
         model.addAttribute("clientes", clientes);
         return "resumen";
+    }
+
+    @GetMapping("/agregar")
+    public String agregar(Cliente cliente){
+        return "modificar";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(@Valid Cliente cliente, Errors errores){
+        if(errores.hasErrors()){
+            return "modificar";
+        }
+        clienteService.guardar(cliente);
+        return "redirect:/";
+    }
+
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Cliente cliente, Model model){
+        cliente = clienteService.encontrarCliente(cliente);
+        model.addAttribute("persona", cliente);
+        return "modificar";
+    }
+
+    @GetMapping("/eliminar")
+    public String eliminar(Cliente cliente){
+        clienteService.eliminar(cliente);
+        return "redirect:/";
     }
 }
