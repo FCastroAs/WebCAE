196289c39cef22d7d159c1241663b88b6719cdbf
Fernán Castro <[fcastro@nalandaglobal.com](mailto:fcastro@nalandaglobal.com)>
2022-05-02 13:22:36 +0200

    Eliminar código inservible

--- src/main/java/edu/uoc/webcae/dao/PersonaDao.java
+++
@@ -1,8 +0,0 @@
-package edu.uoc.webcae.dao;
-
-import edu.uoc.webcae.domain.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaDao extends JpaRepository<Persona, Long>{
-    
-}
