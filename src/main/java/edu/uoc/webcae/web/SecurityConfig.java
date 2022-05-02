d2e61362b2f430693e4a5bc78ca258dc6d2fdd02
Fernán Castro <[fcastro@nalandaglobal.com](mailto:fcastro@nalandaglobal.com)>
2022-05-02 12:14:14 +0200

    Implementar seguridad

--- src/main/java/edu/uoc/webcae/web/SecurityConfig.java
+++ src/main/java/edu/uoc/webcae/web/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/editar/**", "/agregar/**", "/eliminar")
+                .antMatchers("/clientes/**", "/resumen")
                 .hasAnyRole("USER", "ADMIN")
                 .antMatchers("/")
                 .hasAnyRole("USER", "ADMIN")
